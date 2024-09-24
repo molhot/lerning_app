e9277f9789dc910ed3976af115a4aabbc780765f
molhot <sakata19991214@gmail.com>
2024-09-24 14:29:17 +0900

    帳簿アプリのもとを作成

--- tyoubo_project/tyoubo_app/models.py
+++ tyoubo_project/tyoubo_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class ItemModel(models.Model):
+    name = models.CharField('itemname', max_length=200)
+    price = models.IntegerField('price', max_length=140, blank=True)
+    type = models.ChoiceField(
+                choices = (
+                    ('mustitem', '必需品'),
+                    ('notmustitem', '娯楽品'),
+                ),
+                required=True,
+                widget=forms.widgets.Select
+            )
+    date = models.DateField('date', max_length=1000)
--- tyoubo_project/tyoubo_app/views.py
+++ tyoubo_project/tyoubo_app/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import ModelForm
+from tyoubo_app.models import ItemModel
 
-# Create your views here.
+
+def create_post(request):
+    """
+    新たなデータを作成する
+    """
+    post = ItemModel()
+    if request.method == 'GET':
+        form = RegistItemForm(instance=post)
+        return render(request, 'tyoubo_app/tyoubo_page.html', {'form': form})
+    if request.method == 'POST':
+        form = RegistItemForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+        return redirect('tyoubo_app:read_post')
+
+def read_post(request):
+    """
+    データの一覧を表示する
+    """
+    # 全オブジェクトを取得
+    items = ItemModel.objects.all().order_by('id')
+    return render(request,'tyoubo_app/tyoubo_page.html', {'items': items})  # Template に渡すデータ
+
+def edit_post(request, post_id):
+    """
+    対象のデータを編集する
+    """
+    post = get_object_or_404(ItemModel, pk=post_id)
+    if request.method == 'GET':
+        form = RegistItemForm(instance=post)
+        return render(request, 'tyoubo_app/item_regist_page.html', {'form': form, 'post_id': post_id})  # Template に渡すデータ
+    elif request.method == 'POST':
+        form = RegistItemForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+        return redirect('tyoubo_app:tyoubo_page.html')
+
+def delete_post(request, post_id):
+    post = get_object_or_404(ItemModel, pk=post_id)
+    post.delete()
+    return redirect('tyoubo_app:tyoubo_page.html')
+
+class RegistItemForm(ModelForm):
+    """
+    フォーム定義
+    """
+    class Meta:
+        model = ItemModel
+        fields = ('name', 'price', 'type', 'date')
